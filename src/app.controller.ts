0be279cf52079f0d158e9d5a122eaba81f34d8da
rluco <renatoluco@gmail.com>
2026-09-11 13:22:08 -0300

    (chg) fuera todo lo que no fue usado

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,32 +1,8 @@
 import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import type { Response } from 'express';
 import { AppService } from './app.service';
-import { SampleService } from './database/models/sample/sample.service';
-import { CreateSampleDto } from './database/models/sample/dto/create-sample.dto';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private readonly sampleService: SampleService,
-  ) {}
-
-  @Get()
-  root(@Res() res: Response) {
-    const frontendUrl = process.env.FRONTEND_URL;
-    if (frontendUrl) {
-      return res.redirect(302, frontendUrl);
-    }
-    return res.send(this.appService.getHello());
-  }
-
-  @Post('sample')
-  createSample(@Body() body: CreateSampleDto) {
-    return this.sampleService.create(body);
-  }
-
-  @Get('sample')
-  findSamples() {
-    return this.sampleService.findAll();
-  }
+  constructor(private readonly appService: AppService) {}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule, LeaderboardModule],
+  imports: [ConfigModule.forRoot(), LeaderboardModule],
   controllers: [AppController],
   providers: [AppService],
 })
